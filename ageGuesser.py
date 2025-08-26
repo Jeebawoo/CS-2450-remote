4abf6144fbe32640a5d3dfc53bc9372d196bedf4
Hayden J <spikeyslad21@gmail.com>
2025-08-25 19:42:46 -0600

    Fixed fatal bugs, adjusted rand range to proper upper bound

--- ageGuesser.py
+++ ageGuesser.py
@@ -1,17 +1,17 @@
 import random
 
-def main()
+def main():
 	print("Hello, I am going to guess your age!")
 	name = input("What is your name? ")
 	ages = []
 	while True:
-		guess = random.randint(15, 31)
+		guess = random.randint(15, 30)
 		while guess in ages:
-			guess = random.randint(15, 31)
+			guess = random.randint(15, 30)
 		if input(f"Is your age {guess}? Y/N ") == "Y":
 			print(f"Your name is {name} and you are {guess} years old.")
 			return
 		else:
 			print("Rats.")
 			ages.append(guess)
-
+main()
